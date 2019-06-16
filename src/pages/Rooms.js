cc674b6a2dd000290a939f1f8b1bf8202dc55c20
Gerardo Garcia <geraz26@hotmail.com>
2019-06-15 22:57:19 -0500

    Espacio en blanco

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+
 import { Link } from "react-router-dom";
 export default class Rooms extends Component {
   render() {
