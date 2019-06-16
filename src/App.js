8b3fa8e51370576d2795b39dbe6daf59d473d0bb
Gerardo Garcia <geraz26@hotmail.com>
2019-06-15 21:49:13 -0500

    Render del componente Navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "../src/components/Navbar";
 
 import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <>
+      {/* Todos los componentes que se rendericen afuera del switch, van a ser mostrados en todas las paginas */}
+
+      <Navbar />
+
+      {/* el trabajo de Switch es encontrar el primer martch de las routas y mostrar el componente */}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
