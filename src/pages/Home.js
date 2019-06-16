d597a8209f2c0f9d092aed3af9e841ad8f15c8a8
Gerardo Garcia <geraz26@hotmail.com>
2019-06-15 22:57:34 -0500

    Llamado al componente Services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import Services from "../components/Services";
 import { Link } from "react-router-dom";
 
 export default class Home extends Component {
@@ -8,18 +9,22 @@ export default class Home extends Component {
     // Se hace el llamado al componente hero y se pasa la prop hero asi como los "hijos" (children)
     // que en este caso va a ser otro componente
     return (
-      <Hero>
-        {/* Se manda a llamar el componente banner que manda las props title, subtitle y los hijos
+      <>
+        <Hero>
+          {/* Se manda a llamar el componente banner que manda las props title, subtitle y los hijos
         que en este caso es el <Link> */}
-        <Banner
-          title="Luxurious rooms"
-          subtitle="deluxe rooms starting at $299"
-        >
-          <Link to="/rooms" className="btn-primary">
-            Our Rooms
-          </Link>
-        </Banner>
-      </Hero>
+          <Banner
+            title="Luxurious rooms"
+            subtitle="deluxe rooms starting at $299"
+          >
+            <Link to="/rooms" className="btn-primary">
+              Our Rooms
+            </Link>
+          </Banner>
+        </Hero>
+
+        <Services />
+      </>
     );
   }
 }
